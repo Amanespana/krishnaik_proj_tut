1f1dac777d553dd58df70d428423c5d3b33b5c7a
Amanespana <amandeep1552@gmail.com>
2023-10-11 02:19:30 -0400

    added app.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,1 +1,12 @@
 # In this file I will write all the code required to read the data
+
+import os
+import sys
+from src.exception import CustomeException
+from src.logger import logging
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+#will add the code later. For now I moved directly to Tutorial 8
